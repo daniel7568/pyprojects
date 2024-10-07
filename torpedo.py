e72a3e49beeb6d5af97af791b22034492598281c
daniel ronen <daniel.ronen09@gmail.com>
2024-10-07 22:27:43 +0300

    plot e in a a gragh

---
+++ torpedo.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import math
+def seconds_to_hours(seconds):
+    h = seconds//3600
+    left = seconds%3600
+    m  = left//60
+    left = left%60
+    return(h,"Hours",m,"Minutes",left,"Seconds")
+def torpedo_hit2(ship_velocity, torpedo_velocity, ax, ay):
+    def relative_angle(x1, y1, x2, y2):
+        dx = x2 - x1
+        dy = y2 - y1
+        return math.atan2(dy, dx)
+
+    def calculate_new_cartesian_point(x, y, angle, distance):
+        x = x + distance * math.cos(angle)
+        y = y + distance * math.sin(angle)
+        return x, y
+
+    ship_velocity /= 3.6
+    torpedo_velocity /= 3.6
+    dt = 1
+
+    ship_x = [0]
+    ship_y = [0]
+
+    torpedo_x = [ax]
+    torpedo_y = [ay]
+    time = 0
+    while True:
+        time += dt
+        ship_x.append(ship_x[-1] + ship_velocity * dt)
+        ship_y.append(0)
+
+        angle = relative_angle(torpedo_x[-1], torpedo_y[-1], ship_x[-1], ship_y[-1])
+        distance = torpedo_velocity * dt
+        mx, my = calculate_new_cartesian_point(torpedo_x[-1], torpedo_y[-1], angle, distance)
+        torpedo_x.append(mx)
+        torpedo_y.append(my)
+
+        if math.hypot(ship_x[-1] - torpedo_x[-1], ship_y[-1] - torpedo_y[-1]) < 10:  # Assume hit within 10 meters
+            print(f"Hit at time: {seconds_to_hours(time)} ")
+            break
+
+    plt.plot(ship_x, ship_y, label='Ship')
+    plt.plot(torpedo_x, torpedo_y, label='Torpedo')
+    plt.legend()
+    plt.title('Torpedo Hit Simulation')
+    plt.xlabel('X position (m)')
+    plt.ylabel('Y position (m)')
+    plt.grid(True)
+    plt.axis('equal')
+    plt.show()
+
+
+torpedo_hit2(70, 100, 2000, -50000)
