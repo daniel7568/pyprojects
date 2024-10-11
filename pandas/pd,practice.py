dd480e6f1bdf733b9f6a8947d4b150c69cfe81c6
daniel ronen <daniel.ronen09@gmail.com>
2024-10-11 13:38:21 +0300

    3d torpedo

--- pandas/pd,practice.py
+++ pandas/pd,practice.py
@@ -1,3 +1,8 @@
 import pandas as pd
+
 coffee = pd.read_csv("https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/warmup-data/coffee.csv")
-print((coffee))
+result = pd.read_parquet('./data/results.parquet')
+bios = pd.read_csv("./data/bios.csv")
+
+print(coffee)
+#print(coffee.sum([]))
