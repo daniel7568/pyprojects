53fe9eda3388bffd0cdee2869ab3fb8ead4a5e9c
daniel ronen <daniel.ronen09@gmail.com>
2024-10-21 00:07:28 +0300

    panda

--- pandas/pd,practice.py
+++ pandas/pd,practice.py
@@ -1,7 +1,11 @@
 import pandas as pd
-
+import numpy as np
 coffee = pd.read_csv("https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/warmup-data/coffee.csv")
 result = pd.read_parquet('./data/results.parquet')
 bios = pd.read_csv("./data/bios.csv")
 
-print(bios["weight_kg"].max())
+#print(bios.query('name=="Daniel"'))
+#print(bios.query('name.str.startswith("Daniel")'))
+coffee["price"] = np.where(coffee['Coffee Type'] == 'Espresso',3.99,5.99)
+print(coffee.head())
+print(coffee["price"].sum())
