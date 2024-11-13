3a0d91ebfd2cc48255dcefbaeff3052af36acfd2
daniel ronen <daniel.ronen09@gmail.com>
2024-11-13 18:53:24 +0200

    panda

--- scikit-learn/scikit-basic.py
+++ scikit-learn/scikit-basic.py
@@ -1,9 +1,19 @@
 import pandas as pd
+from pandas.io.sas.sas_constants import column_data_length_offset
+from sklearn.linear_model import LinearRegression
 
-
-data = pd.read_csv(r"C:\Users\User\PycharmProjects\pyprojects\scikit-learn\data_files\hate_crimes.csv")
+data = pd.read_csv(r"C:\Users\danie\PycharmProjects\pyprojects\scikit-learn\data_files\hate_crimes.csv")
 voted_trump = data[data['share_voters_voted_trump']>0.5]
 not_voted_trump = data[data['share_voters_voted_trump']<0.5]
-#print(f"vote trump: {voted_trump["avg_hatecrimes_per_100k_fbi"].mean} hate crime per 100K people")
-#print(f"not vote trump: {not_voted_trump["avg_hatecrimes_per_100k_fbi"].mean} hate crime per 100K people")
-print(voted_trump["avg_hatecrimes_per_100k_fbi"].values.mean)
+
+filter_data = data.dropna()
+y = filter_data["avg_hatecrimes_per_100k_fbi"]
+X = filter_data.drop(columns = ["avg_hatecrimes_per_100k_fbi","hate_crimes_per_100k_splc","state"])
+
+print(X.head())
+print(y.head())
+
+
+model = LinearRegression()
+model.fit(X,y)
+print(model.score(X, y))
