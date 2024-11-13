798c5e08e8e1ced3165fa2a76d78309d987c2d3a
daniel ronen <daniel.ronen09@gmail.com>
2024-11-13 18:06:52 +0200

    add from vs code

--- scikit-learn/scikit-basic.py
+++ scikit-learn/scikit-basic.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from pandas.core.interchange.dataframe_protocol import DataFrame
+
 
 data = pd.read_csv(r"C:\Users\User\PycharmProjects\pyprojects\scikit-learn\data_files\hate_crimes.csv")
-voted_trump = DataFrame(data['share_voters_voted_trump']>50])
-not_voted_trump = Data[data['share_voters_voted_trump']<50]
-print(f"vote trump: {voted_trump["avg_hatecrimes_per_100k_fbi"].mean} hate crime per 100K people")
-print(f"not vote trump: {not_voted_trump["avg_hatecrimes_per_100k_fbi"].mean} hate crime per 100K people")
+voted_trump = data[data['share_voters_voted_trump']>0.5]
+not_voted_trump = data[data['share_voters_voted_trump']<0.5]
+#print(f"vote trump: {voted_trump["avg_hatecrimes_per_100k_fbi"].mean} hate crime per 100K people")
+#print(f"not vote trump: {not_voted_trump["avg_hatecrimes_per_100k_fbi"].mean} hate crime per 100K people")
+print(voted_trump["avg_hatecrimes_per_100k_fbi"].values.mean)
