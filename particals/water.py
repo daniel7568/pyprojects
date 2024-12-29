85bf00fa85c8a2b8e51a21ef7382b5d37d7314d4
daniel ronen <daniel.ronen09@gmail.com>
2024-12-29 09:21:11 +0200

    add from vs code

--- particals/water.py
+++ particals/water.py
@@ -2,8 +2,12 @@ import  pyvista as pv
 import numpy as np
 p = np.random.uniform(low = -100, high = 100, size = (15,3))
 print(p)
-colors = [(27, 124, 242),(77, 148, 235),(125, 176, 240),(155, 194, 242),(191, 215, 245)]
-poly = pv.PolyData(p)
 plotter = pv.Plotter()
-plotter.add_mesh(poly)
+poly = pv.PolyData(p)
+plotter.add_mesh(poly, color = (27, 124, 242))
+colors = [(77, 148, 235),(125, 176, 240),(155, 194, 242),(191, 215, 245)]
+for c in colors:
+    for i in range(15)
+    poly = pv.PolyData(p)
+    plotter.add_mesh(poly, color=c)
 plotter.show()
