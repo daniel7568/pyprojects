ae255222d687d1a75a56d4b34129edbaa94b9c93
daniel ronen <daniel.ronen09@gmail.com>
2024-12-29 11:08:38 +0200

    add from vs code

--- particals/water.py
+++ particals/water.py
@@ -5,9 +5,9 @@ print(p)
 plotter = pv.Plotter()
 poly = pv.PolyData(p)
 plotter.add_mesh(poly, color = (27, 124, 242))
-colors = [(77, 148, 235),(125, 176, 240),(155, 194, 242),(191, 215, 245)]
+colors = np.array([(77, 148, 235),(125, 176, 240),(155, 194, 242),(191, 215, 245)])/255
 for c in colors:
-    for i in range(15)
+    p[:,0] -= 10
     poly = pv.PolyData(p)
     plotter.add_mesh(poly, color=c)
 plotter.show()
