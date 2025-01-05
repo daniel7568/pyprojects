e2d7109287a2ddcb7ff9209af9c52637ddd2117a
daniel ronen <daniel.ronen09@gmail.com>
2025-01-05 13:44:21 +0200

    add from vs code

--- projectiles/complex_missiles.py
+++ projectiles/complex_missiles.py
@@ -51,85 +51,87 @@ def intercept_angle(missile_a_mag,missile_pos,target_x,target_y,time,lunched_tim
         return None,None,None
 
 # Main simulation loop
-while target_pos[1] > 0:
-    target_pos +=  target_vel * dt + time_update
-    if t < 10:
-        v_angle = np.arctan2(target_vel[1], target_vel[0])
-        a_vector = np.array([target_a * np.sin(v_angle), target_a * np.cos(v_angle), 0])
-        target_vel = target_vel + grav * dt + a_vector * dt
-    else:
-        rnd = np.random.uniform(low=-3,high=3, size = 2)
-        rnd_vec = np.array([rnd[0],rnd[1],0])
-        target_vel = target_vel + grav * dt + rnd_vec*dt
-        if not launched:
-            normal_x_target_pos.append(target_pos[0])
-            normal_y_target_pos.append(target_pos[1])
-            normal_t_target_pos.append(target_pos[2])
+def run(num):
+    while target_pos[1] > 0:
+        target_pos +=  target_vel * dt + time_update
+        if t < 10:
             v_angle = np.arctan2(target_vel[1], target_vel[0])
+            a_vector = np.array([target_a * np.sin(v_angle), target_a * np.cos(v_angle), 0])
+            target_vel = target_vel + grav * dt + a_vector * dt
+        else:
+            rnd = np.random.uniform(low=-3,high=3, size = 2)
+            rnd_vec = np.array([rnd[0],rnd[1],0])
+            target_vel = target_vel + grav * dt + rnd_vec*dt
+            if not launched:
+                normal_x_target_pos.append(target_pos[0])
+                normal_y_target_pos.append(target_pos[1])
+                normal_t_target_pos.append(target_pos[2])
+                v_angle = np.arctan2(target_vel[1], target_vel[0])
 
-            if len(normal_x_target_pos) > 30:
-                try:
-                    coef = np.polyfit(normal_x_target_pos, normal_y_target_pos, 2)
-                    if coef[0] < 0:
-                        acurate_count += 1
-                        if acurate_count > 2:
-                            launched = True
-                            launched_time = t
-                            missile_pos = missile_pos + np.array([0,0,launched_time])
-                            time_to_max_speed = None
-                            missile_a_vec = None
+                if len(normal_x_target_pos) > 30:
+                    try:
+                        coef = np.polyfit(normal_x_target_pos, normal_y_target_pos, 2)
+                        if coef[0] < 0:
+                            acurate_count += 1
+                            if acurate_count > 2:
+                                launched = True
+                                launched_time = t
+                                missile_pos = missile_pos + np.array([0,0,launched_time])
+                                time_to_max_speed = None
+                                missile_a_vec = None
 
-                            # Try different acceleration magnitudes
-                            for a in range(60, 110):
-                                deg,x,y = intercept_angle(
-                                    a, missile_pos, normal_x_target_pos,
-                                normal_y_target_pos, normal_t_target_pos,lunched_time=launched_time)
-                                if deg is not None:
-                                    missile_a = np.array([a*np.cos(deg),a*np.sin(deg)-9.81,0])
-                                    break
-                            else:
-                                print("can't hit")
-                except Warning:
-                    pass
+                                # Try different acceleration magnitudes
+                                for a in range(60, 110):
+                                    deg,x,y = intercept_angle(
+                                        a, missile_pos, normal_x_target_pos,
+                                    normal_y_target_pos, normal_t_target_pos,lunched_time=launched_time)
+                                    if deg is not None:
+                                        missile_a = np.array([a*np.cos(deg),a*np.sin(deg)-9.81,0])
+                                        break
+                                else:
+                                    print("can't hit")
+                    except Warning:
+                        pass
 
-        elif deg is not None:
-            if np.sqrt((missile_pos[0]-target_pos[0])**2+(missile_pos[1]-target_pos[1])**2)<2:
-                print("missile hit")
-                break
-            if np.sqrt((missile_pos[0]-target_pos[0])**2+(missile_pos[1]-target_pos[1])**2)< min_dis:
-                min_dis = np.sqrt((missile_pos[0]-target_pos[0])**2+(missile_pos[1]-target_pos[1])**2)
+            elif deg is not None:
+                if np.sqrt((missile_pos[0]-target_pos[0])**2+(missile_pos[1]-target_pos[1])**2)<2:
+                    print("missile hit")
+                    break
+                if np.sqrt((missile_pos[0]-target_pos[0])**2+(missile_pos[1]-target_pos[1])**2)< min_dis:
+                    min_dis = np.sqrt((missile_pos[0]-target_pos[0])**2+(missile_pos[1]-target_pos[1])**2)
 
-            missile_pos = missile_pos + missile_vel * dt + time_update
-            missile_vel = missile_vel + missile_a * dt
-            x_missile_pos.append(missile_pos[0])
-            y_missile_pos.append(missile_pos[1])
-            t_missile_pos.append(missile_pos[2])
-            deg_new = np.arctan2(missile_pos[1]-target_pos[1],missile_pos[0]-target_pos[0])
-            if deg_new != deg:
-                if deg_new > deg:
-                    missile_a[0] -= 0.00005*np.abs(deg_new - deg)
-                else:
-                    missile_a[0] += 0.00005*np.abs(deg_new - deg)
+                missile_pos = missile_pos + missile_vel * dt + time_update
+                missile_vel = missile_vel + missile_a * dt
+                x_missile_pos.append(missile_pos[0])
+                y_missile_pos.append(missile_pos[1])
+                t_missile_pos.append(missile_pos[2])
+                deg_new = np.arctan2(missile_pos[1]-target_pos[1],missile_pos[0]-target_pos[0])
+                if deg_new != deg:
+                    if deg_new > deg:
+                        missile_a[0] -= num*np.abs(deg_new - deg)
+                    else:
+                        missile_a[0] += num*np.abs(deg_new - deg)
 
 
-    x_target_pos.append(target_pos[0])
-    y_target_pos.append(target_pos[1])
-    t_target_pos.append(target_pos[2])
-    t += dt
+        x_target_pos.append(target_pos[0])
+        y_target_pos.append(target_pos[1])
+        t_target_pos.append(target_pos[2])
+        t += dt
 
-norm_global = PowerNorm(gamma=1, vmin=0, vmax= max(t_target_pos))
-plt.figure(figsize=(10, 6))
-plt.scatter(x_missile_pos, y_missile_pos,  label="Missile Path", c=t_missile_pos, cmap='inferno', s=5, norm=norm_global)
-p = plt.scatter(x_target_pos, y_target_pos,  label="Target Path",c=t_target_pos, cmap='inferno', s=5, norm=norm_global)
-plt.scatter(x,y,c='k',label="hit point",s=30)
-plt.xlabel("X Position (m)")
-plt.ylabel("Y Position (m)")
-plt.legend()
-plt.colorbar(p,label='Time (s)')
-plt.ylim(top = max(y_target_pos)+20,bottom=-20)
-plt.xlim(left=-20, right=5000)
-plt.title("Target and Missile Trajectories")
-plt.grid(True)
-plt.show()
-print(t)
-print(min_dis)
+    norm_global = PowerNorm(gamma=1, vmin=0, vmax= max(t_target_pos))
+    plt.figure(figsize=(10, 6))
+    plt.scatter(x_missile_pos, y_missile_pos,  label="Missile Path", c=t_missile_pos, cmap='inferno', s=5, norm=norm_global)
+    p = plt.scatter(x_target_pos, y_target_pos,  label="Target Path",c=t_target_pos, cmap='inferno', s=5, norm=norm_global)
+    plt.scatter(x,y,c='k',label="hit point",s=30)
+    plt.xlabel("X Position (m)")
+    plt.ylabel("Y Position (m)")
+    plt.legend()
+    plt.colorbar(p,label='Time (s)')
+    plt.ylim(top = max(y_target_pos)+20,bottom=-20)
+    plt.xlim(left=-20, right=5000)
+    plt.title("Target and Missile Trajectories")
+    plt.grid(True)
+    plt.show()
+    print(t)
+    print(min_dis)
+for i in np.arange(0.000001)
