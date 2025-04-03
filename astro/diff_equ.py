9fd6b32fd1b9a41e0c6ddc64bd5d871465945b91
daniel ronen <daniel.ronen09@gmail.com>
2025-04-03 13:08:36 +0300

    add from vs code

--- astro/diff_equ.py
+++ astro/diff_equ.py
@@ -10,7 +10,16 @@ def calc_next_x(xn,dt):
     k4 = x_dot(xn + k3)*dt
     return xn + (1/6)*(k1+2*k2+2*k3+k4)
 
+def clac_tragectory(x0,f,tf,dt):
+    x_ls = [x0]
+    t_ls = [0]
+    for _ in range(int(tf/dt)):
+        x_new = calc_next_x(x0,dt)
+        x_ls.append(x_new)
+        t_ls.append(t_ls[-1]+dt)
+        x0 = x_new
+    return t_ls,x_ls
+
 x0 = 1
-x_ls = [x0]
-t_ls = [0]
 dt = 0.1
+f = lambda x:x-x**2
